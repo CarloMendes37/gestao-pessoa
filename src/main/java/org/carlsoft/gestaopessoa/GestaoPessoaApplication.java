0f3a9183bd7d13e0c23ff708e310447bb6133f44
Carlos Mendes <carlosmendes37@hotmail.com>
2021-01-28 07:53:33 +0100

    Criação do Projecto

---
+++ src/main/java/org/carlsoft/gestaopessoa/GestaoPessoaApplication.java
@@ -0,0 +1,13 @@
+package org.carlsoft.gestaopessoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoPessoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoPessoaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/carlsoft/gestaopessoa/GestaoPessoaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.carlsoft.gestaopessoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoPessoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
